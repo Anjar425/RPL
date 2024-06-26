f8b99a4d45f41d64af38c9f2fffa4447f623da64
Eko <ekoginanjar4@gmail.com>
2024-06-26 14:52:43 +0700

    add history seeder

--- database/seeders/HistoriesTableSeeder.php
+++ database/seeders/HistoriesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -9,17 +10,32 @@ class HistoriesTableSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('histories')->insert([
+        $now = Carbon::now();
+        $dummyData = [
             [
-                'medicine_id' => 1, // Pastikan id ini sesuai dengan data di medicines
+                'medicine_id' => 1,
                 'date' => '2023-01-01',
                 'type' => 'In',
                 'amount' => 50,
                 'price' => 1000,
-                'created_at' => now(),
-                'updated_at' => now(),
+                'created_at' => $now,
+                'updated_at' => $now,
             ],
-            // Anda bisa menambahkan data lain di sini
-        ]);
+        ];
+
+        // Generate additional 20 dummy records
+        for ($i = 1; $i <= 40; $i++) {
+            $dummyData[] = [
+                'medicine_id' => rand(1, 6), // Assuming you have 10 medicines
+                'date' => Carbon::create(2023, rand(1, 12), rand(1, 28))->toDateString(),
+                'type' => rand(0, 1) ? 'In' : 'Out',
+                'amount' => rand(1, 100),
+                'price' => rand(500, 2000) * rand(1, 100),
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+        }
+
+        DB::table('histories')->insert($dummyData);
     }
 }
